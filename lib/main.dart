2ff4dd491d4bcde84e1e77e15a3104f1cf81e53a
Lucas Xavier <lucasbritesxavier2010@gmail.com>
2025-02-16 15:26:57 -0300

    :sparkles: created component to home page

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,9 @@
-import 'dart:convert';
-
 import 'package:flutter/material.dart';
-import 'package:http/http.dart' as http;
 import 'package:flutter_dotenv/flutter_dotenv.dart';
-import 'package:geolocator/geolocator.dart';
-import 'package:weather_app/classes/weather_data.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:weather_app/l10n/l10n.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:weather_app/modules/home/home.dart';
 
 void main() async {
   await dotenv.load(fileName: '.env');
@@ -19,133 +18,18 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Weather App',
+      locale: const Locale('en'),
+      localizationsDelegates: const [
+        AppLocalizations.delegate,
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+      ],
+      supportedLocales: L10n.all,
+      // TODO dark theme
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
       home: const MyHomePage(title: 'Weather App'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-  Future<WeatherData>? _weatherData;
-
-  @override
-  initState() {
-    super.initState();
-    _weatherData = _fetchWeather();
-  }
-
-  Future<WeatherData> _fetchWeather() async {
-    try {
-      String apiKey = dotenv.env['WEATHER_API_KEY']!;
-      Position position = await Geolocator.getCurrentPosition();
-      print("Latitude: ${position.latitude}, Longitude: ${position.longitude}");
-
-      String latLong =
-          position.latitude.toString() + ',' + position.longitude.toString();
-
-      final response = await http.get(
-        Uri.parse(
-          'http://api.weatherapi.com/v1/current.json?key=$apiKey&q=$latLong&aqi=yes',
-        ),
-      );
-      if (response.statusCode == 200) {
-        print(response.body);
-        return WeatherData.fromJson(jsonDecode(response.body));
-      } else {
-        print('Failed to fetch weather data');
-        throw Exception('Failed to load weather data');
-      }
-    } catch (e) {
-      print('Failed to fetch weather data: $e');
-      throw Exception('Failed to load weather data');
-    }
-  }
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: FutureBuilder<WeatherData>(
-          future: _weatherData,
-          builder: (context, snapshot) {
-            if (snapshot.connectionState == ConnectionState.waiting) {
-              return CircularProgressIndicator();
-            } else if (snapshot.hasError) {
-              return Text('Error: ${snapshot.error}');
-            } else if (snapshot.hasData) {
-              final weather = snapshot.data!;
-              return Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Text('Location: ${weather.location.name} ${weather.location.region} (${weather.location.country})'),
-                  Text('Temperature: ${weather.current.tempC}°C'),
-                  Text(' ${weather.current.condition.text}'),
-                  Image.network('http:${weather.current.condition.icon}'),
-                ],
-              );
-            } else {
-              return Text('No data available');
-            }
-          },
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
---
+++ lib/modules/home/home.dart
@@ -0,0 +1,116 @@
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:geolocator/geolocator.dart';
+import 'package:weather_app/classes/weather_data.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  // This widget is the home page of your application. It is stateful, meaning
+  // that it has a State object (defined below) that contains fields that affect
+  // how it looks.
+
+  // This class is the configuration for the state. It holds the values (in this
+  // case the title) provided by the parent (in this case the App widget) and
+  // used by the build method of the State. Fields in a Widget subclass are
+  // always marked "final".
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  Future<WeatherData>? _weatherData;
+
+  @override
+  initState() {
+    super.initState();
+    _weatherData = _fetchWeather();
+  }
+
+  Future<WeatherData> _fetchWeather() async {
+    try {
+      String apiKey = dotenv.env['WEATHER_API_KEY']!;
+      Position position = await Geolocator.getCurrentPosition();
+      print("Latitude: ${position.latitude}, Longitude: ${position.longitude}");
+
+      String latLong =
+          position.latitude.toString() + ',' + position.longitude.toString();
+
+      final response = await http.get(
+        Uri.parse(
+          'http://api.weatherapi.com/v1/current.json?key=$apiKey&q=$latLong&aqi=yes',
+        ),
+      );
+      if (response.statusCode == 200) {
+        print(response.body);
+        return WeatherData.fromJson(jsonDecode(response.body));
+      } else {
+        print('Failed to fetch weather data');
+        throw Exception('Failed to load weather data');
+      }
+    } catch (e) {
+      print('Failed to fetch weather data: $e');
+      throw Exception('Failed to load weather data');
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    // This method is rerun every time setState is called, for instance as done
+    // by the _incrementCounter method above.
+    //
+    // The Flutter framework has been optimized to make rerunning build methods
+    // fast, so that you can just rebuild anything that needs updating rather
+    // than having to individually change instances of widgets.
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: FutureBuilder<WeatherData>(
+          future: _weatherData,
+          builder: (context, snapshot) {
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              return CircularProgressIndicator();
+            } else if (snapshot.hasError) {
+              return Text('Error: ${snapshot.error}');
+            } else if (snapshot.hasData) {
+              final weather = snapshot.data!;
+              return Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  SizedBox(
+                    width: 250,
+                    child: TextField(
+                      decoration: InputDecoration(
+                        border: OutlineInputBorder(),
+                        labelText: 'Enter a location',
+                      ),
+                    ),
+                  ),
+                  Text(
+                    'Location: ${weather.location.name} ${weather.location.region} (${weather.location.country})',
+                  ),
+                  Text('Temperature: ${weather.current.tempC}°C'),
+                  Text(' ${weather.current.condition.text}'),
+                  Image.network('http:${weather.current.condition.icon}'),
+                ],
+              );
+            } else {
+              return Text('No data available');
+            }
+          },
+        ),
+      ),
+    );
+  }
+}
