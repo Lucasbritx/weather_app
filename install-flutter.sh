ce59fa50a63c319078812c34b051eb797300d7d0
Lucas Xavier <lucasbritesxavier2010@gmail.com>
2025-02-16 19:48:55 -0300

    :green_heart: install flutter sh config

--- install-flutter.sh
+++ install-flutter.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
-# Install Flutter
-git clone https://github.com/flutter/flutter.git -b stable
-export PATH="$PATH:`pwd`/flutter/bin"
-flutter doctor
+set -e
+
+# Baixar e instalar Flutter
+FLUTTER_VERSION="3.29.0" # Substitua pela versão desejada
+curl -O https://storage.googleapis.com/flutter_infra_release/releases/stable/linux/flutter_linux_$FLUTTER_VERSION-stable.tar.xz
+tar xf flutter_linux_$FLUTTER_VERSION-stable.tar.xz
+export PATH="$PATH:$PWD/flutter/bin"
+flutter --version
