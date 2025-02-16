d859d659423f9f2664d68a41b880c8ae489b32ef
Lucas Xavier <lucasbritesxavier2010@gmail.com>
2025-02-16 10:48:43 -0300

    :sparkles: created weather data class

---
+++ lib/classes/weather_data.dart
@@ -0,0 +1,165 @@
+class WeatherData {
+  final Location location;
+  final Current current;
+
+  WeatherData({required this.location, required this.current});
+
+  factory WeatherData.fromJson(Map<String, dynamic> json) {
+    return WeatherData(
+      location: Location.fromJson(json['location']),
+      current: Current.fromJson(json['current']),
+    );
+  }
+}
+
+class Location {
+  final String name;
+  final String region;
+  final String country;
+  final double lat;
+  final double lon;
+  final String tzId;
+  final String localtime;
+
+  Location({
+    required this.name,
+    required this.region,
+    required this.country,
+    required this.lat,
+    required this.lon,
+    required this.tzId,
+    required this.localtime,
+  });
+
+  factory Location.fromJson(Map<String, dynamic> json) {
+    return Location(
+      name: json['name'],
+      region: json['region'],
+      country: json['country'],
+      lat: json['lat'],
+      lon: json['lon'],
+      tzId: json['tz_id'],
+      localtime: json['localtime'],
+    );
+  }
+}
+
+class Current {
+  final double tempC;
+  final double tempF;
+  final int isDay;
+  final Condition condition;
+  final double windMph;
+  final double windKph;
+  final int windDegree;
+  final String windDir;
+  final double pressureMb;
+  final double pressureIn;
+  final double precipMm;
+  final double precipIn;
+  final int humidity;
+  final int cloud;
+  final double feelslikeC;
+  final double feelslikeF;
+  final double uv;
+  final AirQuality airQuality;
+
+  Current({
+    required this.tempC,
+    required this.tempF,
+    required this.isDay,
+    required this.condition,
+    required this.windMph,
+    required this.windKph,
+    required this.windDegree,
+    required this.windDir,
+    required this.pressureMb,
+    required this.pressureIn,
+    required this.precipMm,
+    required this.precipIn,
+    required this.humidity,
+    required this.cloud,
+    required this.feelslikeC,
+    required this.feelslikeF,
+    required this.uv,
+    required this.airQuality,
+  });
+
+  factory Current.fromJson(Map<String, dynamic> json) {
+    return Current(
+      tempC: json['temp_c'],
+      tempF: json['temp_f'],
+      isDay: json['is_day'],
+      condition: Condition.fromJson(json['condition']),
+      windMph: json['wind_mph'],
+      windKph: json['wind_kph'],
+      windDegree: json['wind_degree'],
+      windDir: json['wind_dir'],
+      pressureMb: json['pressure_mb'],
+      pressureIn: json['pressure_in'],
+      precipMm: json['precip_mm'],
+      precipIn: json['precip_in'],
+      humidity: json['humidity'],
+      cloud: json['cloud'],
+      feelslikeC: json['feelslike_c'],
+      feelslikeF: json['feelslike_f'],
+      uv: json['uv'],
+      airQuality: AirQuality.fromJson(json['air_quality']),
+    );
+  }
+}
+
+class Condition {
+  final String text;
+  final String icon;
+  final int code;
+
+  Condition({
+    required this.text,
+    required this.icon,
+    required this.code,
+  });
+
+  factory Condition.fromJson(Map<String, dynamic> json) {
+    return Condition(
+      text: json['text'],
+      icon: json['icon'],
+      code: json['code'],
+    );
+  }
+}
+
+class AirQuality {
+  final double co;
+  final double no2;
+  final double o3;
+  final double so2;
+  final double pm25;
+  final double pm10;
+  final int usEpaIndex;
+  final int gbDefraIndex;
+
+  AirQuality({
+    required this.co,
+    required this.no2,
+    required this.o3,
+    required this.so2,
+    required this.pm25,
+    required this.pm10,
+    required this.usEpaIndex,
+    required this.gbDefraIndex,
+  });
+
+  factory AirQuality.fromJson(Map<String, dynamic> json) {
+    return AirQuality(
+      co: json['co'],
+      no2: json['no2'],
+      o3: json['o3'],
+      so2: json['so2'],
+      pm25: json['pm2_5'],
+      pm10: json['pm10'],
+      usEpaIndex: json['us-epa-index'],
+      gbDefraIndex: json['gb-defra-index'],
+    );
+  }
+}
